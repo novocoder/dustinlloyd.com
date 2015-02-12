adbe1744f1e467746f27a95f1a9a84ba7fdfaff9
root <root@dustin.erulabs.com>
2015-02-12 01:23:50 +0000

    cleaned up personal page

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,13 +1,73 @@
-<html>
-<body>
-<div id= body>
-sadfafsawooooooords
-</div>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Dustin Lloyd: Resume </title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="script.js"></script>
+  </head>
+
+  <body>
+ <div class="container">
+      <div class="header">
+        <h1>Dustin Lloyd</h1>
+        <div>lloyd.dustin@gmail.com</div>
+        <div>San Antonio, TX (210)-744-0224</div>
+        <div class="objective">Passionate self-learner seeking employment in I.T. to further expand my knowledge and bridge the gap between <br> front and back-end web development.
+      </div>
+      </div>
+      <div class="section">
+        <h4>Technical Expertise:</h4>
+        <ul>
+          <li>Installation, configuration, and monitoring of LAMP and LNMP (LEMP) stack systems.</li>
+          <li>Working knowledge of DNS, SSH, HTTP SMTP, IMAP, FTP protocols.</li>
+          <li>Linux Expertise: File systems, permissions, users, firewalls.</li>
+          <li>Web Development experience with Javascript, HTML5, CSS3, PHP and MySQL and blogging platforms such as Ghost and Wordpress.</li>
+          <li>Experience with application monitoring such as NewRelic, Rackspace Cloud Monitoring Agent and manual tools.</li>
+          <li>Experience with Ubuntu, CentOS, RedHat, and Gentoo Linux Operating Systems.</li>
+          <li>Managed code respositories using git and Github.com.</li>
+          <li>Network+ and Cloud U certified. Studying for Linux+ certification.</li>
+        </ul>
+      </div>
+      <div class="section">
+        <h4>Work Experience:</h4>
+            <ul>
+            <li>Video/Photo Editor and Photographer at Lisa Marie Roberts Photography (January 2013 -- January 2014)  Shot weddings and quinceaneras, ran the photo-booth, did photo and video editing as well as build presentations and albums for clients.</li>
+            <li>Instructor & Design Editor at Flash Me Photography (May 2013 – Present)
+Photo editing and Photoshop lessons  taught at photography and Photoshop workshops with 20-30 attendants.</li>
+            <li>Freelance Graphics/Editing/Design (January 2009 – Present)
+Provided 2D and 3D graphic work (brochures, pamphlets, logos, banners, etc.), photo and video editing, web design and management for various clients.</li>
+            </ul>
+      <div class="section">
+        <h4>Hobbies:</h4>
+        <ul>
+	  <li>Throwing myself down the rabbit hole of configuring, breaking, uninstalling, and reinstalling various Linux distros locally and server-side to learn new tools and tricks.
+          <li>Learning new software, making scenes and 3D models and applying them to game engines, preparing them to be printed, and making  still or video composites.</li>
+          <li>Playing the mandolin, competitive gaming with friends, and trying a new IPA or Belgian Ale.</li>
+          <li>Searching for ways to implement code into everyday life.</li>
+        </ul>
+      </div>
+      <div class="section">
+        <h4>Projects:</h4>
+        <ul>
+          <li><a href="http://league.dustinlloyd.com">PHP-Based Web App Connecting to League of Legends API</a></li>
+          <li>
+<input class="toggle-gallery" id="gallery" type="checkbox" >
+<label for="gallery">3D Renders & Composites</label>
+<div><iframe class="imgur-album" width="100%" height="550" frameborder="0" src="//imgur.com/a/lCwAo/embed"></iframe></div></li>
+        </ul>
+      </div>
+      </div>
+    </div>
+  </body>
+
+
+
+
 </html>
-sdfsdfsfd
-<?php
 
 
 
-?>
+
+
